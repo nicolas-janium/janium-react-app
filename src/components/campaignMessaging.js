7fdbdb44fb8e1ca440caf5c753d83238857c16be
Evan Glickman <esglickman@gmail.com>
2021-06-02 19:13:34 -0600

    messaging set up and views almost done

--- src/components/campaignMessaging.js
+++ src/components/campaignMessaging.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Typography, Box, Paper, Tabs, Tab, Card, CardHeader, CardActions, CardContent, Button, TextField, Checkbox, FormControlLabel, Link, Select, FormControl, MenuItem }  from '@material-ui/core';
+import { AppBar, Typography, Button }  from '@material-ui/core';
 import { EmailMessageDiv, LinkedInMessageDiv } from "../components";
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,7 +11,13 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: 7,
         padding: "10px 8px",
         display: "flex",
-        flexDirection: "column"
+        flexDirection: "column",
+        maxWidth: 1250,
+        margin: "0 auto"
+    },
+    buttonClass: {
+        width: "60%",
+        margin: "0 auto"
     }
 }));
 
@@ -30,19 +36,22 @@ export default function CampaignMessaging(props) {
                     tab (to the left). Once associated, Janium will take care of the rest of the steps after connection.
                 </Typography>
             </div>
-            <div className={classes.messagesContainer}>
+            <div className={classes.messagesContainer + " tableBoxShadow"}>
                 <LinkedInMessageDiv stepTitle="Step 2: Linkedin Welcome Message" placeholderText="Enter LinkedIn Welcome Message text here..." />
                 <br />
-                <EmailMessageDiv stepTitle="Step 3: Email Follow-up #1" placeholderText="Enter Email Follow-up #1 message text here..." />
+                <EmailMessageDiv stepTitle="Step 3: Email Follow-up #1" emailSubject="Email #1 Subject" placeholderText="Enter Email Follow-up #1 message text here..." />
                 <br />
                 <LinkedInMessageDiv stepTitle="Step 4: Linkedin Follow-up #1" placeholderText="Enter LinkedIn Follow-up #1 message text here..." />
                 <br />
-                <EmailMessageDiv stepTitle="Step 5: Email Follow-up #2" placeholderText="Enter Email Follow-up #2 message text here..." />
+                <EmailMessageDiv stepTitle="Step 5: Email Follow-up #2" emailSubject="Email #2 Subject" placeholderText="Enter Email Follow-up #2 message text here..." />
                 <br />
                 <LinkedInMessageDiv stepTitle="Step 6: Linkedin Follow-up #2" placeholderText="Enter LinkedIn Follow-up #2 message text here..."/>
                 <br />
-                <EmailMessageDiv stepTitle="Step 7: Email Follow-up #3" placeholderText="Enter Email Follow-up #3 message text here..." />
+                <EmailMessageDiv stepTitle="Step 7: Email Follow-up #3" emailSubject="Email #3 Subject" placeholderText="Enter Email Follow-up #3 message text here..." />
                 <br />
+                <Button variant="contained" color="primary" className={classes.buttonClass + " mt-3"}>
+                    Save Campaign
+                </Button>
             </div>
         </div>
     );
--- src/components/emailMessageDiv.js
+++ src/components/emailMessageDiv.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, TextField } from '@material-ui/core';
+// import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+    },
+    divWrapper: {
+        minWidth: 550,
+        marginLeft: "auto"
     },
     textFieldStyles: {
         background: "#FFF",
@@ -18,22 +25,49 @@ const useStyles = makeStyles((theme) => ({
         "&:focus": {
             outline: "unset"
         }
+    },
+    emailSubjectStyles: {
+        minWidth: 200
+    },
+    richTextEditor: {
+        background: "#FFF"
     }
 }));
 
 export default function EmailMessageDiv(props) {
     const classes = useStyles();
-    
+
     return (
-        <div>
+        <div className={classes.divWrapper}>
             <Typography variant="subtitle1" gutterBottom>
                 {props.stepTitle}
             </Typography>
-            <br />
-            <div>
-                <TextField className={classes.textFieldStyles} id="outlined-basic" label="LI Delay" variant="outlined" size="small" />
+            <div className="d-flex justify-content-between">
+                <TextField className={classes.textFieldStyles + " " + classes.emailSubjectStyles} id="outlined-basic" label={props.emailSubject} variant="outlined" size="small" />
                 <TextField className={classes.textFieldStyles} id="outlined-basic" label="LI Delay" variant="outlined" size="small" />
             </div>
+
+            <CKEditor
+                editor={ BalloonEditor }
+                config={{placeholder: props.placeholderText}}
+                // onReady={ editor => {
+                //     // You can store the "editor" and use when it is needed.
+                //     console.log( 'Editor is ready to use!', editor );
+                // } }
+                // onChange={ ( event, editor ) => {
+                //     const data = editor.getData();
+                //     console.log( { event, editor, data } );
+                // } }
+                // onBlur={ ( event, editor ) => {
+                //     console.log( 'Blur.', editor );
+                // } }
+                onFocus={ ( event, editor ) => {
+                    console.log( 'Focus.', editor );
+                } }
+                onClick={ (event, editor) => {
+                    const data = editor.getData();
+                }}
+            />
         </div>
     );
 }
